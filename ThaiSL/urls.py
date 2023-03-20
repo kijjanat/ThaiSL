d525c21b075fd09441c3a2c05a6ece14bdc0ba85
kijjanat <90678630+kijjanat@users.noreply.github.com>
2023-03-20 13:54:16 +0700

    Add files via upload

---
+++ ThaiSL/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('animation/',views.animation_view,name='animation'),
+    path('',views.home_view,name='home'),
+    path('image/', views.image_view, name='image'),
+	path('pronoun/', views.pronoun_view, name='pronoun'),
+	path('verb/', views.verb_view, name='verb'),
+	path('most/', views.most_view, name='most'),
+	path('emotional/', views.emotional_view, name='emotional'),
+	path('place/', views.place_view, name='place'),
+	path('cons/', views.cons_view, name='cons'),
+	path('vowel/', views.vowel_view, name='vowel'),
+	path('tone/', views.tone_view, name='tone'),
+]
